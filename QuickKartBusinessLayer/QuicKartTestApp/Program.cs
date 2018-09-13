4e9683d03c248d0c62c18aa0efab26a9e9ad8069
gurpreetsingh1300 <gurpreetsinghsethi13@gmail.com>
2018-09-13 14:28:53 +0530

    changes

---
+++ QuickKartBusinessLayer/QuicKartTestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickKartBusinessLayer;
+
+namespace QuicKartTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer custObj = new Customer();
+
+            custObj.customerName = "Fred";
+            custObj.customerAddress = "Las Vegas";
+            custObj.customerContact = "+1-231-34452";
+
+            custObj.MyProfile();
+        }
+    }
+}
---
+++ QuickKartBusinessLayer/QuickKartBusinessLayer/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBusinessLayer
+{
+    // makes it accessible
+    public class Customer
+    {
+        //Member Variables for Customer
+        public string customerName;
+        public string customerAddress;
+        public string customerContact;
+
+        //A method which displays the Customer details
+        public void MyProfile()
+        {
+            Console.WriteLine("Customer Name:" + customerName);
+            Console.WriteLine("Customer Address:" + customerAddress);
+            Console.WriteLine("Customer Contact:" + customerContact);
+        }
+    }
+}
